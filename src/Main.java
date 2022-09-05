a92dcbf8db1960bd48d9c7283fe45b683d335c07
Iyad-Ibrahim <iyad@live.ca>
2022-09-05 18:31:35 -0400

    Tested implementation of Queue data structure using a LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
@@ -8,6 +10,7 @@ public class Main {
 		// push() adds to the top of the stack
 		// pop() removes from the top of the stack
 		
+		/*
 		Stack<String> stack = new Stack<String>();
 		
 		System.out.println(stack.isEmpty()); //isEmpty() returns true if stack is empty and false otherwise
@@ -26,7 +29,38 @@ public class Main {
 		System.out.println(stack.peek()); //peek() returns the top element without removing it from the stack
 		
 		System.out.println(stack.search("Overwatch")); //search() returns the index of the searched element in the stack or -1 if element is not found
+		*/
 		
+		//----------------------------------------------------------------------
+		
+		// Queue is FIFO data structure (First In First Out)
+		// enqueue(), offer() ---> adds to the tail of the queue
+		// dequeue(), poll() ---> removes the head of the queue
+		
+		/*
+		Queue<String> q = new LinkedList<String>();
+		
+		System.out.println(q.isEmpty()); //isEmpty returns true if q is empty and false otherwise
+		
+		q.offer("Mom");
+		q.offer("Dad");
+		q.offer("Bro");
+		q.offer("Me");
+		
+		System.out.println(q);
+		
+		System.out.println(q.peek()); //peek returns head of the queue without removing it from queue
+		
+		System.out.println(q.size()); //size() returns the size of the queue
+		
+		System.out.println(q.contains("Mom")); //contains() returns true if queue contains element and false otherwise
+		
+		q.poll(); //element() does the same as poll but throws an exception if queue is empty
+		
+		System.out.println(q);
+		*/
+		
+		//----------------------------------------------------------------------
 		
 		
 
